468b24fe3775744f54efcdd67e8094b36fc4b0d7
Netson <netsoncavina@gmail.com>
2022-06-30 02:56:14 -0300

    added professor tests

--- app.test.js
+++ app.test.js
@@ -1,4 +1,3 @@
-const { response } = require("express");
 const request = require("supertest");
 const app = require("./app");
 
@@ -144,6 +143,166 @@ describe("Alunos", () => {
       let ra = "0";
       return request(app)
         .delete(`/alunos/${ra}`)
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body).toEqual(
+            expect.objectContaining({
+              acknowledged: true,
+              deletedCount: 1,
+            })
+          );
+        });
+    });
+  });
+});
+
+describe("Professores", () => {
+  describe("POST tests", () => {
+    it("POST /professores --> Cadastra um professor", () => {
+      return request(app)
+        .post("/professores")
+        .send({
+          nome: "Teste",
+          formacao: "Teste",
+          dataNascimento: "01/01/01",
+          email: "teste@teste.com",
+          cpf: 12345678911,
+        })
+        .expect("Content-Type", /json/)
+        .expect(201)
+        .then((response) => {
+          expect(response.body).toEqual(
+            expect.objectContaining({
+              __v: expect.any(Number),
+              _id: expect.any(String),
+              nome: "Teste",
+              formacao: "Teste",
+              dataNascimento: expect.any(String),
+              email: "teste@teste.com",
+              cpf: 12345678911,
+            })
+          );
+        });
+    });
+  });
+  describe("GET tests", () => {
+    it("GET /professores --> Array de professores", () => {
+      return request(app)
+        .get("/professores")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((response) => {
+          expect.arrayContaining([
+            expect.objectContaining({
+              __v: expect.any(Number),
+              _id: expect.any(String),
+              nome: expect.any(String),
+              formacao: expect.any(String),
+              dataNascimento: expect.any(String),
+              email: expect.any(String),
+              cpf: expect.any(Number),
+            }),
+          ]);
+        });
+    });
+
+    it("GET /professores/:cpf --> Professor especifico por CPF", () => {
+      let cpf = "12345678911";
+      return request(app)
+        .get(`/professores/${cpf}`)
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({
+                __v: expect.any(Number),
+                _id: expect.any(String),
+                nome: "Teste",
+                formacao: "Teste",
+                dataNascimento: expect.any(String),
+                email: "teste@teste.com",
+                cpf: 12345678911,
+              }),
+            ])
+          );
+        });
+    });
+
+    it("GET /professores/:cpf --> Array vazio se não encontrado", () => {
+      let cpf = "09909909909";
+      return request(app)
+        .get(`/professores/${cpf}`)
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body).toEqual(expect.arrayContaining([]));
+        });
+    });
+
+    it("GET /professores/professor/:nome --> Professor especifico por nome", () => {
+      let nome = "Teste";
+      return request(app)
+        .get(`/professores/professor/${nome}`)
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({
+                __v: expect.any(Number),
+                _id: expect.any(String),
+                nome: "Teste",
+                formacao: "Teste",
+                dataNascimento: expect.any(String),
+                email: "teste@teste.com",
+                cpf: 12345678911,
+              }),
+            ])
+          );
+        });
+    });
+
+    it("GET /professores/professor/:nome --> Array vazio se nome não encontrado", () => {
+      let nome = "Teste";
+      return request(app)
+        .get(`/professores/professor/${nome}`)
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body).toEqual(expect.arrayContaining([]));
+        });
+    });
+  });
+  describe("PATCH tests", () => {
+    it("PATCH /professores/:cpf --> atualiza os dados de um professor", () => {
+      let cpf = 12345678911;
+      return request(app)
+        .patch(`/professores/${cpf}`)
+        .send({
+          formacao: "Tester",
+        })
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body).toEqual(
+            expect.objectContaining({
+              acknowledged: true,
+              modifiedCount: 1,
+              upsertedId: null,
+              upsertedCount: 0,
+              matchedCount: 1,
+            })
+          );
+        });
+    });
+  });
+  describe("DELETE tests", () => {
+    it("DELETE /professores/:cpf --> Excluir um professor", () => {
+      let cpf = 12345678911;
+      return request(app)
+        .delete(`/professores/${cpf}`)
         .expect("Content-Type", /json/)
         .expect(200)
         .then((response) => {
